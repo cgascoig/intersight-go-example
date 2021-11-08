83251f5c2e400f517b36c19e2b61958c003f475d
Chris Gascoigne <cgascoig@cisco.com>
2021-11-09 10:20:48 +1100

    add comments and error checking

--- main.go
+++ main.go
@@ -14,10 +14,13 @@ func main() {
 	keyFile := os.Getenv("IS_KEY_FILE")
 
 	config := intersight.NewConfiguration()
-	config.Debug = false
+
+	// Uncomment this line if you want to see the Intersight API requests/responses
+	// config.Debug = true
 
 	client := intersight.NewAPIClient(config)
 
+	// Set up the authentication configuration struct
 	authConfig := intersight.HttpSignatureAuth{
 		KeyId:          keyID,
 		PrivateKeyPath: keyFile,
@@ -32,13 +35,19 @@ func main() {
 		SigningAlgorithm: intersight.HttpSigningAlgorithmRsaPKCS1v15,
 	}
 
+	// Get a context that includes the authentication config
 	authCtx, err := authConfig.ContextWithValue(context.Background())
 	if err != nil {
 		log.Fatal("Error creating authentication context")
 	}
 
+	// Execute the GetNtpPolicyList operation
 	res, _, err := client.NtpApi.GetNtpPolicyList(authCtx).Execute()
+	if err != nil {
+		log.Fatalf("Error getting NTP policy list: %v", err)
+	}
 
+	// Loop through the NtpPolicy list and print the names
 	for _, ntpPolicy := range res.NtpPolicyList.Results {
 		fmt.Printf("Ntp Policy: %s\n", *ntpPolicy.Name)
 	}
